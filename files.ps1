87348851e63c7fadc0f68a72efec99031f627495
Michael Madden <mikemadden42@icloud.com>
2023-06-06 12:31:20 -0500

    Initial files.ps1

---
+++ files.ps1
@@ -0,0 +1,11 @@
+# copy an item
+# Copy-Item file1 file2
+
+# move an item
+# Move-Item file1 file2
+
+# delete an item
+# Remove-Item file1
+
+# remove an entire directory recursively
+# Remove-Item -Recurse -Force dir1
